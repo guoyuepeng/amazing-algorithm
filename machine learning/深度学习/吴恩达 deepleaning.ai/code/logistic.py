11646462b4dd70f4aabcc77a8dde511193ed68f7
bulebob <mdattack@126.com>
2018-08-14 04:03:58 -0700

    update

--- machine learning/深度学习/吴恩达 deepleaning.ai/code/logistic.py
+++ machine learning/深度学习/吴恩达 deepleaning.ai/code/logistic.py
@@ -1,4 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+
+# %matplotlib inline
 # GRADED FUNCTION: initialize_with_zeros
 
 def initialize_with_zeros(dim):
@@ -239,3 +247,19 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate
          "num_iterations": num_iterations}
     
     return d
+
+
+def main():
+    
+    # Loading the data (cat/non-cat)
+    train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+    train_set_x = train_set_x_flatten/255.
+    test_set_x = test_set_x_flatten/255.
+    d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, 
+                learning_rate = 0.005, print_cost = True)
+    
+
+if __name__ == '__main__':
+    main()
