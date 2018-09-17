20bb7e0a2f80378137179b93946ad1743a1a7782
bulebob <mdattack@126.com>
2018-09-16 19:29:13 -0700

    update

---
+++ 推荐系统/推荐系统实战读书笔记/实践代码/python/利用用户行为数据/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import os
+print(os.getcwd())
+
+if __name__ == '__main__':
+
+    ratings = pd.read_table("../../../dataset/ml-1m/ratings.dat", sep="::", header=None,
+                            names=["userid", "movieid", "rating", "timestamp"])
+    users = pd.read_table("../../../dataset/ml-1m/users.dat", sep="::", header=None,
+                          names=["userid", "gender", "age", "occupation"])
+    movies = pd.read_table("../../../dataset/ml-1m/movies.dat", sep="::", header=None,
+                           names=["title", "genres"])
+
+    # 目标：预测用户是否会对电影评分
+    # 构建隐式反馈的数据集
+    dataset = ratings[['userid', 'movieid']]
+
+    # user-based
+
+    # item-based
+
+    print("execute sucessfully!")
---
+++ 推荐系统/推荐系统实战读书笔记/实践代码/python/利用用户行为数据/similarity.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+def jaccard():
+
+
+
+    return
+
+
+def cosine():
+
+
+    return
