5b3919e7933f589514b59aad78c04c7125c17942
bulebob <mdattack@126.com>
2018-11-20 03:57:58 -0800

    update

---
+++ code/python-basic/statsmodels_basic.py
@@ -0,0 +1,19 @@
+# https://github.com/statsmodels/statsmodels
+
+# very powerful!
+
+# Linear regression models(Quantile regression,Weighted least squares...)
+# 
+
+import numpy as np
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+# Load data
+dat = sm.datasets.get_rdataset("Guerry", "HistData").data
+
+# Fit regression model (using the natural log of one of the regressors)
+results = smf.ols('Lottery ~ Literacy + np.log(Pop1831)', data=dat).fit()
+
+# Inspect the results
+print(results.summary())
